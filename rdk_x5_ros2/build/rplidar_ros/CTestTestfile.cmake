b5397dd3134e52a8e5f616ac4d76f0c19ad98bf7
liaojingwu20041031 <liaojingwu20041031@github.com>
2026-04-01 22:35:12 +0800

    fix: Update hardcoded workspace paths from ROS2_ylhb to ROS2-smart-car/rdk_x5_ros2

---
+++ rdk_x5_ros2/build/rplidar_ros/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ljw/ROS2-smart-car/rdk_x5_ros2/src/rplidar_ros-ros2
+# Build directory: /home/ljw/ROS2-smart-car/rdk_x5_ros2/build/rplidar_ros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
